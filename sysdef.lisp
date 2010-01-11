fe3770c7771b7dd43a83805aa22bdcd268927071
Andy Hefner <ahefner@gmail.com>
2010-01-11 17:51:51 +0700

    We depend on libvorbisfile now.

--- sysdef.lisp
+++ sysdef.lisp
@@ -4,8 +4,9 @@
 (defun cflags () (list "-I/usr/include/freetype2" "-std=c99"))
 
 (defun c-sources ()
-  "C source code modules"
+  "C source code modules."
   (list "src/sys.c"
+        "src/audio.c"
         "src/text-render.c"))
 
 (defun lisp-compile-sources ()
@@ -15,7 +16,7 @@
         "src/globals.lisp"))
 
 (defun lisp-sources ()
-  "Lisp sources to be linked into the final executable"
+  "Lisp sources to be linked into the final executable."
   (list "sysdef.lisp"               ; For runtime recompile feature..
         "src/package.lisp"
         "src/globals.lisp"
@@ -38,5 +39,6 @@
 (defun shared-libraries ()
   (list "SDL" "SDL_image"
         "GL"
+        "vorbisfile"
         "freetype" "z"))
 
